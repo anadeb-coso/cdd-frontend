1158479b455c3c419c8e167946bc984f517477ba
Freddy <freddyrp16@gmail.com>
2022-08-04 00:35:52 +0200

    Configured "Mark as Complete" button in task detail

--- src/screens/TaskDetail.tsx
+++ src/screens/TaskDetail.tsx
@@ -1,27 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Heading, Progress, ScrollView, Text } from 'native-base';
-import { TouchableOpacity, View, ImageBackground } from 'react-native';
+import {
+  Box,
+  Heading,
+  ScrollView,
+  Stack,
+  Text,
+  Modal,
+  Input,
+  FormControl,
+  Button,
+  VStack,
+} from 'native-base';
+
+import { TouchableOpacity, View, Image } from 'react-native';
 import { Layout } from '../components/common/Layout';
 import LocalDatabase from '../utils/databaseManager';
 
 function TaskDetail({ route }) {
   const task = route.params?.task;
   const [tasks, setTasks] = useState([]);
+  const [showCompleteModal, setShowCompleteModal] = useState(false);
+  const [showToProgressModal, setShowToProgressModal] = useState(false);
+
+  const updateTask = () => {
+    // eslint-disable-next-line no-underscore-dangle
+    LocalDatabase.upsert(task._id, function (doc) {
+      doc = task;
+      return doc;
+    })
+      .then(function (res) {
+        setShowCompleteModal(false);
+        setShowToProgressModal(false);
+      })
+      .catch(function (err) {
+        console.log('Error', err);
+        // error
+      });
+  };
 
   return (
     <Layout disablePadding>
-      <ScrollView _contentContainerStyle={{ pt: 7, px: 5, flexGrow: 1 }}>
-        <Box
-          // maxW="80"
-          rounded="lg"
-          p={3}
-          flex={1}
-          justifyContent={'space-between'}
-          // overflow="hidden"
-          bg="white"
-          // borderWidth="1"
-          shadow={1}
-        >
+      <ScrollView _contentContainerStyle={{ pt: 7, px: 5, flexGrow: 1, pb: 7 }}>
+        <Stack px="5">
           <Heading my={3} fontWeight="bold" size="sm">
             {task.name}
           </Heading>
@@ -29,24 +49,143 @@ function TaskDetail({ route }) {
           <Text fontSize="sm" color="gray.600">
             {task.description}
           </Text>
-        </Box>
+        </Stack>
 
+        <View style={{ flex: 1 }}>
+          <Image
+            resizeMode="stretch"
+            style={{ height: 100, width: undefined }}
+            source={require('../../assets/backgrounds/horizontal-blue.png')}
+          />
+          <Box
+            top={7}
+            position="absolute"
+            px={7}
+            rounded="lg"
+            // p={3}
+            // mt={3}
+            flexDirection="row"
+            justifyContent="space-evenly"
+            bg="transparent"
+            // shadow={1}
+          >
+            <View style={{ flex: 3 }}>
+              <Heading fontWeight="bold" size="xs" color="white">
+                Supporting Materials
+              </Heading>
+              <Text fontSize="sm" color="white">
+                Click to view
+              </Text>
+            </View>
+            <Box
+              justifyContent="center"
+              alignItems="center"
+              flex={1}
+              rounded="lg"
+              backgroundColor="rgba(2,3,6,0.3)"
+            >
+              <Text fontWeight="bold" fontSize="8" color="white">
+                Viewed
+              </Text>
+              <Heading fontWeight="bold" size="sm" color="white">
+                0/5
+              </Heading>
+            </Box>
+          </Box>
+        </View>
+        <Modal
+          isOpen={showCompleteModal}
+          onClose={() => setShowCompleteModal(false)}
+          size="lg"
+        >
+          <Modal.Content maxWidth="400px">
+            <Modal.Header>
+              Do you wish to mark this task as completed?
+            </Modal.Header>
+
+            <Modal.Body>
+              <VStack space="sm">
+                <Button
+                  rounded="xl"
+                  onPress={() => {
+                    task.completed = true;
+                    updateTask();
+                  }}
+                >
+                  MARQUÉE COMME EN COURS
+                </Button>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowCompleteModal(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </VStack>
+            </Modal.Body>
+          </Modal.Content>
+        </Modal>
+        <Modal
+          isOpen={showToProgressModal}
+          onClose={() => setShowToProgressModal(false)}
+          size="lg"
+        >
+          <Modal.Content maxWidth="400px">
+            <Modal.Header>
+              Do you wish to mark this task as in progress?
+            </Modal.Header>
+
+            <Modal.Body>
+              <VStack space="sm">
+                <Button
+                  rounded="xl"
+                  onPress={() => {
+                    task.completed = false;
+                    updateTask();
+                  }}
+                >
+                  YES, MARK AS IN PROGRESS
+                </Button>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowToProgressModal(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </VStack>
+            </Modal.Body>
+          </Modal.Content>
+        </Modal>
         <TouchableOpacity
+          onPress={() => {
+            if (task.completed) {
+              setShowToProgressModal(true);
+            } else {
+              setShowCompleteModal(true);
+            }
+          }}
           style={{ flexDirection: 'row', justifyContent: 'center' }}
         >
           <Box
             py={3}
-            px={6}
+            px={8}
             mt={6}
-            bg="transparent"
+            bg={task.completed ? 'yellow.500' : 'primary.500'}
             rounded="xl"
             borderWidth={1}
-            borderColor="primary.500"
+            borderColor={task.completed ? 'yellow.500' : 'primary.500'}
             justifyContent="center"
             alignItems="center"
           >
-            <Text fontWeight="bold" fontSize="2xs" color="primary.500">
-              Complete
+            <Text fontWeight="bold" fontSize="xs" color="white">
+              {task.completed
+                ? 'MARK AS IN PROGRESS'
+                : 'MARQUÉE COMME TERMINÉE'}
             </Text>
           </Box>
         </TouchableOpacity>

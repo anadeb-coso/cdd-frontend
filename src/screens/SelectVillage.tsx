7f561491ad0002fe454ce43febe1d235c3d9938b
adaboubvincent <adaboubvincent@gmail.com>
2023-02-20 15:53:15 +0000

    Removed the comment for the progressbar

--- src/screens/SelectVillage.tsx
+++ src/screens/SelectVillage.tsx
@@ -176,29 +176,6 @@ function SelectVillage() {
         )}
       />
 
-{/* <View style={styles.container}>
-      <View style={styles.example}>
-        <Text>Circle Progress Indicator</Text>
-        <ProgressBarAndroid />
-      </View>
-      <View style={styles.example}>
-        <Text>Horizontal Progress Indicator</Text>
-        <ProgressBarAndroid styleAttr="Horizontal" />
-      </View>
-      <View style={styles.example}>
-        <Text>Colored Progress Indicator</Text>
-        <ProgressBarAndroid styleAttr="Horizontal" color="#2196F3" />
-      </View>
-      <View style={styles.example}>
-        <Text>Fixed Progress Value</Text>
-        <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          progress={0.5}
-        />
-      </View>
-    </View> */}
-
     </Layout>
   );
 }

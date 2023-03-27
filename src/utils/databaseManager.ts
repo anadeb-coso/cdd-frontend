b8d20b98c8dd71b29a92146ef9d27e7ca649ce08
adaboubvincent <adaboubvincent@gmail.com>
2023-03-27 10:20:55 +0000

    Puted couchdb ids

--- src/utils/databaseManager.ts
+++ src/utils/databaseManager.ts
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import PouchDB from 'pouchdb-react-native';
 import PouchAuth from 'pouchdb-authentication';
 import PouchAsyncStorage from 'pouchdb-adapter-asyncstorage';
-import AuthContext from '../contexts/auth';
+// import AuthContext from '../contexts/auth';
 
 PouchDB.plugin(PouchAuth);
 PouchDB.plugin(require('pouchdb-upsert'));
@@ -28,7 +28,20 @@ export const SyncToRemoteDatabase = async ({
     });
   }
   // console.log(username, password);
+
+
+  /*
+  I added this with the intention of solving the problem of non accessibility of the 
+  couchdb (this happens after a few days after connectivity to the mobile application) 
+  for the future if it can solve the problem while waiting for another alternative.
+  */
+  no_sql_user = "Edrrxr4ZPXVgjtCj";
+  no_sql_pass = "peggi0-caGbah-pacdat";
+  // no_sql_user = "root";
+  // no_sql_pass = "root";
+
   const remoteDB = new PouchDB(`http://54.151.2.224:5984/${no_sql_db_name}`, {
+  // const remoteDB = new PouchDB(`http://10.0.2.2:5984/${no_sql_db_name}`, {
     skip_setup: true,
   });
   try {
@@ -53,11 +66,12 @@ export const SyncToRemoteDatabase = async ({
         }
       });
     });
+
     return true;
   } catch (e) {
     console.log('Error!:', e);
-    const { signOut } = useContext(AuthContext);
-    signOut();
+    // const { signOut } = useContext(AuthContext);
+    // signOut();
     
     return false;
   }

faa9746e2d47a1444c3cd5b0bf5a6ae62b1da1a6
adaboubvincent <adaboubvincent@gmail.com>
2023-08-03 15:15:48 +0000

    Setuped function allowing to update facilitator form datas on couchdb

--- src/services/API.tsx
+++ src/services/API.tsx
@@ -1,7 +1,7 @@
 // const baseURL = 'http://52.52.147.181/';
 // const baseURL = 'https://cddanadeb.e3grm.org/';
-const baseURL = 'http://cdd-env.eba-mz2nppu7.us-west-1.elasticbeanstalk.com/';
-// const baseURL = 'http://10.0.2.2:8001/';
+// const baseURL = 'http://cdd-env.eba-mz2nppu7.us-west-1.elasticbeanstalk.com/';
+const baseURL = 'http://10.0.2.2:8001/';
 export { baseURL };
 function handleErrors(response) {
   if (response.non_field_errors) {
@@ -30,5 +30,27 @@ class API {
       .catch(error => ({ error }));
     return result;
   }
+
+
+  async sync_datas(data) {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(data),
+    };
+    const result = fetch(
+      `${baseURL}process_manager/save-form-datas/`,
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(handleErrors)
+      .then(a => a)
+      .catch(error => ({ error }));
+    return result;
+  }
+
+
 }
 export default API;

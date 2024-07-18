7206b417a45680688ae87f307fe540536fcc7292
adaboubvincent <adaboubvincent@gmail.com>
2024-07-18 19:41:26 +0000

    Set up Phases colors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -27,43 +27,63 @@ export const DONATIONS = [
 
 
 export const TYPES_OF_SUB_PROJECT_COLOR = {
-    'Bâtiment Scolaire au Pré-scolaire': "#808080", 
-    'Bâtiment Scolaire au Primaire': "#808080", 
-    'Bâtiment Scolaire au CEG': "#808080", 
-    'Bâtiment Scolaire au Lycée': "#808080", 
-    
-    'Forage Photovoltaïque (Boisson)': "#0000ff", 
-    'Forage Photovoltaïque (Centre communautaire)': "#0000ff", 
-    'Forage Photovoltaïque (Ecole)': "#0000ff", 
-    'Forage Photovoltaïque (Latrines)': "#0000ff", 
-    'Forage Photovoltaïque (Maison des jeunes)': "#0000ff", 
-    'Forage Photovoltaïque (Maraichage)': "#0000ff", 
-    'Forage Photovoltaïque (Salle de réunion)': "#0000ff", 
-    "Pompe à motricité humaine (PMH)": "#1e90ff", 
-    "Retenue d'eau": "#601ee0", 
+    'Bibliothèques scolaires': '#808080', //Gray f-Gris
+    'Blocs de latrines dans les établissements scolaires': '#808080', //Gray f-Gris
+    'Bâtiment Scolaire au CEG': '#808080', //Gray f-Gris
+    'Bâtiment Scolaire au Lycée': '#808080', //Gray f-Gris
+    'Bâtiment Scolaire au Primaire': '#808080', //Gray f-Gris
+    'Bâtiment Scolaire au Pré-scolaire': '#808080', //Gray f-Gris
+    'CMS': '#008200', //Green f-vert
+    "Cantine d'Hôpital": "#008200", //Green f-vert
+    'Centre Communautaire': "#ffa500", //Orange f-orange
+    'Clôture (Centre de santé)': "#ffa500", //Orange f-orange
+    'Clôture (Ecole)': '#808080', //Gray f-Gris
+    "Dalot d'accès à l'école": '#808080', //Gray f-Gris
+    'Electrification hors réseau avec lampadaires solaires': "#9c9c14", //DarkYellow f-jaune sombre
+    'Extension réseau électrique': "#939301", //Olive f-Olive
+    'Forage Photovoltaïque (Boisson)': '#0000ff', //Blue f-blue
+    'Forage Photovoltaïque (Centre communautaire)': '#0000ff',  //Blue f-blue
+    'Forage Photovoltaïque (Ecole)': '#0000ff',  //Blue f-blue
+    'Forage Photovoltaïque (Maison des jeunes)': '#0000ff',  //Blue f-blue
+    'Forage Photovoltaïque (Maraichage)': '#0000ff',  //Blue f-blue
+    'Forage Photovoltaïque (Salle de réunion)': '#0000ff',  //Blue f-blue
+    'Incinérateurs médicaux': "#008200", //Green f-vert
+    'Laboratoire': "#00ff00", //Lime f-citron vert
+    'Latrine Communautaire': "#191970", //MidNightBlue f-Blue sombre
+    'Magasin de Stockage': "#800080", //Purple f-Violet
+    'Maison des jeunes': "#ff0000", //Red f-rouge
+    'Paillote enseignants': '#808080', //Gray f-Gris
+    'Paillote pour centre de santé': "#008200", //Green f-vert
+    'Pharmacie': "#006400", //Darkgreen f-vert sombre
+    'Pistes': "#000000", //Black f-noire
+    'Pédiatrie': "#92d492", //DarkSeaGreen f-Vert de mer foncé
+    'Reboisement': "#deb887", //Burlywood f-Bois massif
+    "Retenue d'eau": '#601ee0', //Blueviolet f-Blue violet
+    'Réhabilitation PMH': '#1e90ff', //DodgerBlue f-Bleu cagnard
+    'Réhabilitation PMH en Forage Photovoltaïque (Ecole)': '#1e90ff', //DodgerBlue f-Bleu cagnard
+    'Salle de réunion': "#a75e06", //DarkOrange f-Orange sombre
+    'Terrain de Foot': "#7a1212", //DarkRed f-rouge sombre
+    'USP': "#3b7024", //DarkOliveGreen f-Vert olive foncé
+}
 
-    'CMS': "#008200", 
-    'CHP': "#32cd32", 
-    'USP': "#3b7024", 
-    'Pharmacie': "#006400", 
-    'Pédiatrie': "#92d492", 
-    'Laboratoire': "#00ff00", 
 
-    
-    'Extension réseau électrique': "#939301", 
-    'Lampadaires solaire': "#9c9c14", 
+export const PHASES_COLORS = [
+    '#D9D9D9', // 0
+    '#63D3AC', // 1
+    '#F0788E', // 2
+    '#F2CD86', // 3
+    '#9095FF', // 4
+    '#44967D', // 5
+    '#BA79B7', // 6
+    '#E9B9C2', // 7
+]
 
-    'Centre Communautaire': "#ffa500", 
-    'Salle de réunion': "#a75e06", 
-
-    'Maison des jeunes': "#ff0000", 
-    'Terrain de Foot': "#7a1212", 
-
-    'Magasin De Stockage': "#800080", 
-    
-    'Reboisement': "#deb887", 
-
-    'Piste/OF': "#000000", 
-
-    'Latrine Communautaire': "#191970", 
+export const PHASES_WITH_THEIR_NUMBERS = {
+    "VISITES PREALABLES": 1,
+    "MOBILISATION COMMUNAUTAIRE": 2,
+    "PLANIFICATION": 3,
+    "PRÉPARATION SOUS-PROJET": 4,
+    "CONSULTATION  ET EXAMEN SOUS-PROJET": 5,
+    "MISE EN ŒUVRE DU SOUS-PROJET": 6,
+    "CLOTURE ET REPLANIFICATION DU SOUS-PROJET": 7,
 }

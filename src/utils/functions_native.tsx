3e0006fb852c7456e85fc5658d6cc8116cc63a49
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2026-02-09 16:04:25 +0000

    Updated natve functions file

--- src/utils/functions_native.tsx
+++ src/utils/functions_native.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Image, Alert } from 'react-native';
+import { Image, Alert, Platform, ToastAndroid, View, Dimensions } from 'react-native';
 import * as FileSystem from 'expo-file-system';
 // import RNFS from 'react-native-fs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { PDFDocument, rgb } from 'pdf-lib';
 import * as Location from 'expo-location';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import * as mime from 'react-native-mime-types';
 import { range } from './functions';
+import { couchDBURLBase } from './databaseManager';
+
 
 export const getImageDimensions = async (imageUri: string) => {
   return new Promise((resolve, reject) => {
@@ -22,19 +26,22 @@ export const getImageDimensions = async (imageUri: string) => {
 };
 
 
-export const getImageSize = async (imageUri: string) => {
+export const getImageSize = async (imageUri: string): Promise<number> => {
   let fileSizeInMB = 0;
   try {
     const fileInfo = await FileSystem.getInfoAsync(imageUri);
-    const fileSizeInBytes = fileInfo.size;
-    fileSizeInMB = fileSizeInBytes ? fileSizeInBytes / (1024 * 1024) : 0; // Convert bytes to MB
-    //   console.log('Image size:', fileSizeInMB, 'MB');
+
+    if (fileInfo.exists && fileInfo.size) {
+      fileSizeInMB = fileInfo.size / (1024 * 1024); // Convert bytes to MB
+    }
+
+    // console.log('Image size:', fileSizeInMB, 'MB');
   } catch (error) {
     console.error('Error getting image size:', error);
   }
+
   return fileSizeInMB;
 };
-
 
 
 // export const clearCache = async () => {
@@ -200,83 +207,83 @@ const readPDF = async (uri: string) => {
 //   return outputUri;
 // };
 
-export const compressPDF = async (uri: string, compress: number = 0.5) => {
-  const _compressPDF = async (pdfData: any, compress: number = 0.5) => {
-    const pdfDoc = await PDFDocument.load(pdfData);
+// export const compressPDF = async (uri: string, compress: number = 0.5) => {
+//   const _compressPDF = async (pdfData: any, compress: number = 0.5) => {
+//     const pdfDoc = await PDFDocument.load(pdfData);
 
-    // Compress images in the PDF
-    const pages = pdfDoc.getPages();
-    for (const page of pages) {
-      const { width, height } = page.getSize();
-      const newPage = pdfDoc.addPage([width, height]);
+//     // Compress images in the PDF
+//     const pages = pdfDoc.getPages();
+//     for (const page of pages) {
+//       const { width, height } = page.getSize();
+//       const newPage = pdfDoc.addPage([width, height]);
 
-      newPage.drawPage(page);
+//       newPage.drawPage(page);
 
-      const images = page.node.Resources.XObject || {};
-      for (const key in images) {
-        const image = images[key];
-        if (image && image.embedImages) {
-          const [imageStream] = await image.embedImages();
-          const imageDims = imageStream.scale(compress);
+//       const images = page.node.Resources.XObject || {};
+//       for (const key in images) {
+//         const image = images[key];
+//         if (image && image.embedImages) {
+//           const [imageStream] = await image.embedImages();
+//           const imageDims = imageStream.scale(compress);
 
-          newPage.drawImage(imageStream, {
-            x: 0,
-            y: 0,
-            width: imageDims.width,
-            height: imageDims.height,
-          });
-        }
-      }
+//           newPage.drawImage(imageStream, {
+//             x: 0,
+//             y: 0,
+//             width: imageDims.width,
+//             height: imageDims.height,
+//           });
+//         }
+//       }
 
-      pdfDoc.removePage(pdfDoc.getPageCount() - 2);
-    }
+//       pdfDoc.removePage(pdfDoc.getPageCount() - 2);
+//     }
 
-    // Remove metadata
-    pdfDoc.setProducer('');
-    pdfDoc.setCreator('');
+//     // Remove metadata
+//     pdfDoc.setProducer('');
+//     pdfDoc.setCreator('');
 
-    const compressedPdfBytes = await pdfDoc.saveAsBase64({ dataUri: true });
-    return compressedPdfBytes;
-  };
+//     const compressedPdfBytes = await pdfDoc.saveAsBase64({ dataUri: true });
+//     return compressedPdfBytes;
+//   };
 
-  const saveCompressedPDF = async (compressedPdfData: any, outputUri: string) => {
-    try {
-      await FileSystem.writeAsStringAsync(outputUri, compressedPdfData.split(',')[1], { encoding: FileSystem.EncodingType.Base64 });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   const saveCompressedPDF = async (compressedPdfData: any, outputUri: string) => {
+//     try {
+//       await FileSystem.writeAsStringAsync(outputUri, compressedPdfData.split(',')[1], { encoding: FileSystem.EncodingType.Base64 });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  const compressAndSavePDF = async (inputUri: string, outputUri: string) => {
-    const pdfData = await readPDF(inputUri);
-    const compressedPdfData = await _compressPDF(pdfData, compress);
-    await saveCompressedPDF(compressedPdfData, outputUri);
-  };
+//   const compressAndSavePDF = async (inputUri: string, outputUri: string) => {
+//     const pdfData = await readPDF(inputUri);
+//     const compressedPdfData = await _compressPDF(pdfData, compress);
+//     await saveCompressedPDF(compressedPdfData, outputUri);
+//   };
 
-  const readPDF = async (uri: string) => {
-    try {
-      const pdfData = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
-      return pdfData;
-    } catch (error) {
-      console.error('Error reading PDF:', error);
-      throw error;
-    }
-  };
+//   const readPDF = async (uri: string) => {
+//     try {
+//       const pdfData = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+//       return pdfData;
+//     } catch (error) {
+//       console.error('Error reading PDF:', error);
+//       throw error;
+//     }
+//   };
 
-  const inputUri: string = uri;
-  const fileNameWithExtension = uri.split('/')[uri.split('/').length - 1];
-  const fileName = fileNameWithExtension.split('.')[0];
-  const fileExtension = fileNameWithExtension.split('.')[1];
-  const outputFileNameWithExtension = `${fileName}_output.${fileExtension}`;
-  const outputUri: string = [
-    ...(uri.split('/').filter((e: any, i: number) => i != uri.split('/').length - 1)),
-    outputFileNameWithExtension
-  ].join("/");
+//   const inputUri: string = uri;
+//   const fileNameWithExtension = uri.split('/')[uri.split('/').length - 1];
+//   const fileName = fileNameWithExtension.split('.')[0];
+//   const fileExtension = fileNameWithExtension.split('.')[1];
+//   const outputFileNameWithExtension = `${fileName}_output.${fileExtension}`;
+//   const outputUri: string = [
+//     ...(uri.split('/').filter((e: any, i: number) => i != uri.split('/').length - 1)),
+//     outputFileNameWithExtension
+//   ].join("/");
 
-  await compressAndSavePDF(inputUri, outputUri);
+//   await compressAndSavePDF(inputUri, outputUri);
 
-  return outputUri;
-};
+//   return outputUri;
+// };
 
 
 
@@ -302,3 +309,92 @@ export const covered_location = async () => {
     });
 
 }
+
+
+export const getFileType = async (url: string, username: string, password: string) => {
+    const filename = url.split('/').pop();
+    const mimeTypeFromExt = filename ? mime.lookup(filename) : null;
+
+    if (mimeTypeFromExt) return mimeTypeFromExt;
+
+    // Si extension inconnue, tente HEAD
+    let config = {}
+    if(!["", null, undefined].includes(username)){
+      const base64Creds = btoa(`${username}:${password}`);
+        config = {
+          Authorization: `Basic ${base64Creds}`,
+          username: username,
+          password: password,
+        }
+      }
+    const res = await fetch(url, {
+        method: 'HEAD',
+        headers: config
+    });
+
+    return res.headers.get('Content-Type');
+};
+
+export const downloadToDownloadsFolder = async (url: string, username: string, password: string, progressCallback  = (arg?: any) => null) => {
+    try {
+    
+        if (!url.includes("http")) {
+            url = couchDBURLBase + url;
+        }
+
+        const url_split = url.split("?")[0].split("/");
+        const filename = url_split[url_split.length - 1];
+
+        const dirs = ReactNativeBlobUtil.fs.dirs; // Chemin vers le dossier "Downloads" (Android) ou "Documents" (iOS)
+
+        // Android: utilise le dossier Downloads
+        // iOS: utilise le dossier Documents (car iOS n'autorise pas l'accès direct à Downloads)
+
+        const downloadDir = Platform.OS === 'android' ? dirs.DownloadDir : dirs.DocumentDir;
+
+        const path = `${downloadDir}/${filename}`;
+
+        const fileExists = await ReactNativeBlobUtil.fs.exists(path);
+        
+        if (!fileExists) {
+            // console.log("Téléchargement vers:", path);
+
+            let config = {}
+            if(!["", null, undefined].includes(username)){
+              const base64Creds = btoa(`${username}:${password}`);
+              config = {
+                Authorization: `Basic ${base64Creds}`,
+                username: username,
+                password: password,
+              }
+            }
+
+            // Télécharge le fichier
+            const mime = await getFileType(filename, username, password);
+            const res = await ReactNativeBlobUtil.config({
+                fileCache: true,
+                path,
+                addAndroidDownloads: {
+                    useDownloadManager: true, // Utilise le gestionnaire de téléchargement Android (notification + stockage visible)
+                    notification: true,
+                    title: `Téléchargement de ${filename}`,
+                    description: "Fichier en cours de téléchargement...",
+                    mime: mime ? mime : undefined, // (Optionnel) Définir le type MIME (ex: 'application/pdf')
+                },
+            }).fetch('GET', url, config).progress((received: any, total: any) => {
+                const progress = received / total;
+                // console.log(`Progression: ${(progress * 100).toFixed(2)}%`);
+                progressCallback(progress);
+            });
+
+            // console.log("Fichier téléchargé avec succès:", path);
+            return path;
+        }
+
+    } catch (error) {
+        console.error("Erreur de téléchargement:", error);
+        progressCallback(-1); // Signal d'erreur
+        // throw error;
+    }
+    return null;
+};
